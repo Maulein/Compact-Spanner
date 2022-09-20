f1d88cda225b81d0dc142a8dc313f130545f9954
Maulein Pathak <112417206+Maulein@users.noreply.github.com>
2022-09-20 17:32:11 +0530

    Update Spanner.cc

--- FGV_Baseline/Spanner.cc
+++ FGV_Baseline/Spanner.cc
@@ -1,6 +1,7 @@
 //MIT License
 
 //Copyright (c) 2022 Maulein
+// This code is part of our work titled “Scalable algorithms for compact low-stretch spanners on real-world graphs"
 
 // This work is adapted from the work done under project "Theoretically Efficient Parallel Graph
 // Algorithms Can Be Fast and Scalable", presented at Symposium on Parallelism
